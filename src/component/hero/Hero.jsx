f770bc74779122bac9174d0bae5f607851fc213d
Nico <nicolasromero.barrios@gmail.com>
2025-01-20 17:39:03 -0300

    first update

---
+++ src/component/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import {React} from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <img
+        src="/hero/starman.png"
+        alt="e-mzil"
+        />
+    </div>
+  )
+}
+
+export default Hero
