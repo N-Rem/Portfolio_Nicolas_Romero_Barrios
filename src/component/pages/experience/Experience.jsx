6213c381af644be1ff41d08dc5831b46545b9403
Nico <nicolasromero.barrios@gmail.com>
2025-03-06 17:57:52 -0300

    add exp

--- src/component/pages/experience/Experience.jsx
+++ src/component/pages/experience/Experience.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import './experience.css';
-
+import React from "react";
+import "./experience.css";
+import CardExp from "../../cardExp/CardExp";
+import CardsExpSlider from "../../cardExp/cardsExpSlider";
 
 const Experience = () => {
-  return (
-    <div className='exp-container flex-container-column'>
-      <h2 id='Exp'>Experiencia</h2>
-      <div>
-        Componente de Slider de Tarjetas de experiencia
-      </div>
-    </div>
-  )
-}
+  
 
-export default Experience
+  return (
+    <div className="exp-container flex-container-column">
+      <h2 id="Exp">Experiencia</h2>
+      <CardsExpSlider />
+    </div>
+  );
+};
+
+export default Experience;
